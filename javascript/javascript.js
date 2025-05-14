0d10a92fde4dd78db186f2391a32889eaac6d223
Antience100 <190290455+Antience100@users.noreply.github.com>
2025-05-14 18:44:29 -0400

    Added flex container and buttons to HTML, added functionality to clear grid button, centered grid

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,12 +1,23 @@
 const gridContainer = document.getElementById("grid-container");
+const gridSizeBtn = document.getElementById("grid-size");
+const gridClearBtn = document.getElementById("clear-grid");
+
+gridClearBtn.addEventListener("click", () => {clearGrid();});
 
 function buildGrid() {
     for (let i = 0; i < 256; i++) {
         const gridItem = document.createElement("div");
         gridItem.classList.add("grid-item");
         gridContainer.appendChild(gridItem);
-        gridItem.addEventListener("mouseover", (event) => {gridItem.style.backgroundColor="gray";});
-    }
-}
+        gridItem.addEventListener("mouseover", () => {gridItem.style.backgroundColor="gray";});
+    };
+};
+
+function clearGrid() {
+    const gridItemsToClear = document.querySelectorAll(".grid-item");
+    gridItemsToClear.forEach(gridItemToClear => {
+        gridItemToClear.style.backgroundColor="white";
+    });
+};
 
 buildGrid();
