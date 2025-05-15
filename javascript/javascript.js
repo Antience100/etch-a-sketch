3302e7b68185174c963f2a2e1d64a2b4da3384c8
Antience100 <190290455+Antience100@users.noreply.github.com>
2025-05-14 21:42:13 -0400

    completed project -non extra credit version

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,22 +2,55 @@ const gridContainer = document.getElementById("grid-container");
 const gridSizeBtn = document.getElementById("grid-size");
 const gridClearBtn = document.getElementById("clear-grid");
 
-gridClearBtn.addEventListener("click", () => {clearGrid();});
+// Set maximum default player grid size choice to 100
+let playerSizeChoice = 100;
+// Set maximum default grid size to 10000 grid items (100 x 100)
+let gridMax = 10000;
 
-function buildGrid() {
-    for (let i = 0; i < 256; i++) {
-        const gridItem = document.createElement("div");
-        gridItem.classList.add("grid-item");
-        gridContainer.appendChild(gridItem);
-        gridItem.addEventListener("mouseover", () => {gridItem.style.backgroundColor="gray";});
-    };
-};
+gridClearBtn.addEventListener("click", () => {
+  clearGrid();
+});
+gridSizeBtn.addEventListener("click", () => {
+  getPlayerChoice();
+});
+
+function getPlayerChoice() {
+  playerSizeChoice = prompt("Enter a number between 1 and 100");
+  if (playerSizeChoice <= 100 && playerSizeChoice >= 2) {
+    gridSize(playerSizeChoice);
+  } else {
+    alert(
+      "Must be a number greater than or equal to 2, and less than or equal to 100"
+    );
+    getPlayerChoice();
+  }
+}
+
+function buildGrid(gridMaxSize) {
+  let gridItemSize = 1000 / gridMaxSize;
+  for (let i = 0; i < gridMaxSize * gridMaxSize; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
+    gridItem.style.width = `${gridItemSize}px`;
+    gridItem.style.height = `${gridItemSize}px`;
+    gridContainer.appendChild(gridItem);
+    gridItem.addEventListener("mouseover", () => {
+      gridItem.style.backgroundColor = "gray";
+    });
+  }
+}
+
+function gridSize(size) {
+  const gridContainerSize = Math.min(playerSizeChoice, 100);
+  gridContainer.innerHTML = "";
+  buildGrid(gridContainerSize);
+}
 
 function clearGrid() {
-    const gridItemsToClear = document.querySelectorAll(".grid-item");
-    gridItemsToClear.forEach(gridItemToClear => {
-        gridItemToClear.style.backgroundColor="white";
-    });
-};
+  const gridItemsToClear = document.querySelectorAll(".grid-item");
+  gridItemsToClear.forEach((gridItemToClear) => {
+    gridItemToClear.style.backgroundColor = "white";
+  });
+}
 
-buildGrid();
+buildGrid(playerSizeChoice);
